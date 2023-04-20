35df944139eafde80b9d1c43435ca70616a060b1
Francesco Battaglini <francesco.battaglini@lexicondigital.com.au>
2023-04-20 11:18:30 +0000

    Initial commit

---
+++ src/main/java/net/battaglini/ipaddemo/IPadDemoSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package net.battaglini.ipaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPadDemoSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPadDemoSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/battaglini/ipaddemo/IPadDemoSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.battaglini.ipaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPadDemoSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
